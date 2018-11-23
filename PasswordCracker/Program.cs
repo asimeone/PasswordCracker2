d6827dbd76c438b6305cfd8632368277525f803c
Alex Simeone <asimeone18@gmail.com>
2018-11-23 13:27:04 -0500

    adding cracker

--- PasswordCracker/Program.cs
+++ PasswordCracker/Program.cs
@@ -1,4 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
 
 namespace PasswordCracker
 {
@@ -6,9 +12,114 @@ namespace PasswordCracker
     {
         static void Main(string[] args)
         {
-            // read creds.creds
-            //throw it in a dict
-            //
+            Dictionary<string, string> users = new Dictionary<string, string>();
+            CreateUserAndPasswordDictionary(users);
+
+            List<string> dictionaryWords = new List<string>();
+            dictionaryWords = GetWordSubset();
+
+            var md5 = System.Security.Cryptography.MD5.Create();
+
+            bool done = false;
+            bool passwordCracked = false;
+            while (!done || !passwordCracked)
+            {
+                foreach(KeyValuePair<string, string> user in users)
+                {
+                    Console.WriteLine($"User {user.Key} ... starting timer...");
+                    Stopwatch stopwatch = new Stopwatch();
+                    stopwatch.Start();
+
+                    string base64Hash = user.Value;
+
+                    passwordCracked = false;
+
+                    
+                    foreach(string word in dictionaryWords)
+                    {
+                        var wordBytes = Encoding.UTF8.GetBytes(word);
+                        var hash = md5.ComputeHash(wordBytes);
+                        var base64WordHash = Convert.ToBase64String(hash);
+
+                        if(base64Hash.Equals(base64WordHash))
+                        {
+                            passwordCracked = true;
+                            Console.WriteLine($"Password found: {word}");
+                            stopwatch.Stop();
+                            Console.WriteLine($"time it took in seconds: {stopwatch.Elapsed.Seconds}");
+                            Console.WriteLine("Check next user? Y or N");
+                            if (Console.ReadLine().Equals("Y"))
+                            {
+                                done = true;
+                            }
+                        }
+
+                        if (passwordCracked)
+                        {
+                            break;
+                        }
+                            
+                    }
+
+
+                    if(passwordCracked)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.Write($"Password not cracked for user {user}");
+                        Console.WriteLine("Continue? Y or N");
+                        if (Console.ReadLine().Equals("Y"))
+                        {
+                            done = true;
+                        }
+                    }
+                    
+
+                }
+
+                
+            }
+
+        }
+
+        private static List<string> GetWordSubset()
+        {
+            List<string> dictionaryWords;
+            var assembly = Assembly.GetExecutingAssembly();
+            var resourceName = "PasswordCracker.dictionary.txt";
+
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                string result = reader.ReadToEnd();
+                dictionaryWords = result.Split(Environment.NewLine).ToList();
+            }
+
+            return dictionaryWords;
+        }
+
+        private static void CreateUserAndPasswordDictionary(Dictionary<string, string> users)
+        {
+            string path = @"C:\creds.creds";
+
+            var contents = File.ReadAllText(path);
+
+            if (!string.IsNullOrEmpty(contents))
+            {
+                string[] fileContents = contents.Split(Environment.NewLine);
+
+                foreach (string u in fileContents)
+                {
+                    string[] userAndPass = u.Split('|');
+
+                    if (!string.IsNullOrEmpty(userAndPass[0]) && !string.IsNullOrEmpty(userAndPass[1]))
+                    {
+                        users.Add(userAndPass[0], userAndPass[1]);
+                    }
+                }
+            }
         }
     }
 }
